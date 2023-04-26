963e62d1e398db3920f6c069d950e38659a24217
duke_yzl <1057505847@qq.com>
2023-04-26 17:52:55 +0800

    jsapi的获取签名

--- src/main/java/com/yu1998/yutools/utils/WxCommonUtil.java
+++ src/main/java/com/yu1998/yutools/utils/WxCommonUtil.java
@@ -2,6 +2,7 @@ package com.yu1998.yutools.utils;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.yu1998.yutools.bean.menu.Menu;
@@ -104,7 +105,7 @@ public class WxCommonUtil {
         String wx_ticket = YuToolsRedisUtil.get(yuToolsConfig.getWx().getRedisJsapiTicketKey());
         if ((StrUtil.isEmpty(wx_ticket))){
             this.getAccessTokenUri();
-            String result = HttpUtil.get(StrUtil.format(yuToolsConfig.getWx().getJsapiTicketUri(),this.accessToken));
+            String result = HttpRequest.get(StrUtil.format(yuToolsConfig.getWx().getJsapiTicketUri(),this.accessToken)).execute().body();
             JSONObject jSONResult = JSONUtil.parseObj(result);
             YuToolsRedisUtil.set(yuToolsConfig.getWx().getRedisJsapiTicketKey(),jSONResult.getStr("ticket"),jSONResult.getInt("expires_in")-5, TimeUnit.SECONDS);
             wx_ticket = jSONResult.getStr("ticket");
